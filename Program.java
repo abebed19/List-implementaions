da2909c6f0c229ffd43271495ef18440185fc8df
abebed19 <abebed19@gmail.com>
2026-03-11 23:58:06 +0300

    List of numbers

--- Program.java
+++ Program.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
+        
             List<String> myList = new List<>();
             System.out.println(myList.contains("hello"));
             myList.add("hello");
@@ -14,6 +15,9 @@ public class Program {
             System.out.println(myList.value(index));
             myList.remove("hello");
             System.out.println(myList.contains("hello"));
+            List<Integer> numbers = new List<>();
+            numbers.add(90);
+            System.out.print(numbers);
 
     }
 
